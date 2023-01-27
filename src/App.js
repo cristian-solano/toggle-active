971372d8bd72f04c8a216f9177d1200b1f62b6b2
Cristian Solano <cristiansolano1941@gmail.com>
2023-01-27 14:05:24 -0500

    toggle-active

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [stateItem, setStateItem] = useState({
+    activeObjects: null,
+    objects: [{id: 1, name: "Principal"}, {id: 2, name: "Planes"}, {id:3, name: "Contacto"}, {id: 4, name: "Ingreso"}]
+  })
+
+  const active = (index) =>  {
+    setStateItem({...stateItem, activeObjects: stateItem.objects[index]})
+  }
+
+  const activeStyle = (index) => {
+    if (stateItem.objects[index] === stateItem.activeObjects){
+      return "title active";
+    } else {
+      return "title inactive";
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {stateItem.objects.map((element, index) => (
+        <div key={index} className={activeStyle(index)} onClick={() => active(index)}>
+          <p>{element.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
